6faed66fb360d93e88273a3fb4323528ec54cda4
geri35 <geri@upi.edu>
2024-09-14 19:41:06 +0700

    perbaikan about

--- src/components/tentang/judul.jsx
+++ src/components/tentang/judul.jsx
@@ -6,12 +6,12 @@ function Judul() {
     <>
       <div className="flex flex-column p-5 justify-between border-b-2 border-black uppercase font-libre font-bold">
         <div className="p-1 border-2 border-black hover:bg-black hover:text-white">
-          <Link to="/" className="">
+          <Link to="/" className="sm:text-lg text-[15px]">
             Kembali
           </Link>
         </div>
 
-        <div className="text-xl p-1">About</div>
+        <div className="text-xl p-1 sm:text-lg text-[15px]">About</div>
         <div className="p-1">
           <img src="/copy-logo.svg" alt="just-logo" className="w-7 h-7" />
         </div>
@@ -19,7 +19,7 @@ function Judul() {
       <div className="flex justify-center font-libre uppercase p-3 border-b-2 border-black">
         <div className="text-6xl">news</div>
       </div>
-      <div className="flex justify-between p-3 font-libre sm:text-[15px] text-[8px] uppercase border-b-2 border-black">
+      <div className="flex justify-between sm:p-3 p-2  font-libre sm:text-[15px] text-[7px] uppercase border-b-2 border-black">
         <p>Art</p>
         <p>Computer Science</p>
         <p>Student</p>
